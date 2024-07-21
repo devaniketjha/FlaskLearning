00bcc7d7ee65ff7568bab58d971694870f5f3bfa
Aniket Jha <therealaniketjha@gmail.com>
2024-07-21 23:49:37 +0530

    Added navbar to respective pages. Error Pages won't have navbar. That's final.

--- hello.py
+++ hello.py
@@ -6,23 +6,24 @@ app = Flask(__name__)
 
 # Create a route decorator
 @app.route("/")
-
 def index():
-    return "Hello World."
+    return render_template("index.html")
+
 
 @app.route("/user/<name>")
-
 def user(name):
-    
-    return render_template("index.html", name=name)
+    return render_template("user.html", name=name)
+
 
 @app.route('/error')
 def error():
     abort(500)  # This will raise a 500 Internal Server Error
 
+
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template("404.html"), 404
+
 
 @app.errorhandler(500)
 def page_not_found(e):
