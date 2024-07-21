f6f2bc207b36b0857857773d0749f472da9f57fb
Aniket Jha <therealaniketjha@gmail.com>
2024-07-22 01:24:59 +0530

    Added a name page. This will be extended later to something bigger. Testing it for now. I am done for the day.

--- hello.py
+++ hello.py
@@ -1,15 +1,65 @@
-from flask import Flask, render_template, abort
+from flask import Flask, render_template, abort, flash
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+from dotenv import load_dotenv
+import os
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+# Load the .env file
+load_dotenv()
+
 
 
 # Crete a Flask Instance
 app = Flask(__name__)
+
+# Add Database
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+
+# Use absolute path to avoid relative path issues
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'users.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_ECHO'] = True  # Enable SQLAlchemy echo for debugging
+
+# Access the environment variables
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+
+# Initialize the Database
+db = SQLAlchemy(app)
+
+# Create Model
+class Users(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String(200), nullable = False)
+    email = db.Column(db.String(120), nullable = False, unique = True)
+    date_added = db.Column(db.DateTime, default = datetime.now)
+    
+    # Create a String
+    def __repr__(self):
+        return '<Name %r' %self.name
 
 # Create a route decorator
 @app.route("/")
 def index():
     return render_template("index.html")
 
-
+@app.route("/name", methods = ['GET', 'POST'])
+def name():
+    name = None
+    form = NamerForm()
+    # Validate Form
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
+        flash("Form Submitted Successfully")
+    return render_template("name.html",
+                           name = name,
+                           form = form)
+    
+    
 @app.route("/user/<name>")
 def user(name):
     return render_template("user.html", name=name)
@@ -28,6 +78,44 @@ def page_not_found(e):
 @app.errorhandler(500)
 def page_not_found(e):
     return render_template("500.html"), 500
+
+
+# Create a Form Class
+class NamerForm(FlaskForm):
+    name = StringField("What's Your Name", validators=[DataRequired()])
+    submit = SubmitField("Submit")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 if __name__ == '__main__':
--- test.py
+++ test.py
@@ -1,1 +1,7 @@
 # This is for testing purposes only.
+
+from hello import app, db
+
+with app.app_context():
+    db.create_all()
+    print("Database tables created!")
